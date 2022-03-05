e9511b3d8e0fe40bab8c55be3666443d82cfa2b2
棟幹 尹鎭煜 <71953982+y00njinuk@users.noreply.github.com>
2022-03-05 21:39:17 +0900

    add Kotlin exercises for Chapter1

---
+++ kotlin/src/Chapter1/KotlinFirstImpression.kt
@@ -0,0 +1,17 @@
+package Chapter1
+
+data class Person(val name: String, val age: Int? = null)
+
+fun main(args: Array<String>) {
+    val persons = listOf(Person("영희"), Person("철수", age=29))
+    val oldest = persons.maxByOrNull { it.age ?: 0 }
+    println("나이가 가장 많은 사람 : $oldest")
+}
+
+/*
+    @Deprecated("Use maxByOrNull instead.", ReplaceWith("this.maxByOrNull(selector)"))
+    @DeprecatedSinceKotlin(warningSince = "1.4", errorSince = "1.5", hiddenSince = "1.6")
+    public inline fun <T, R : Comparable<R>> Iterable<T>.maxBy(selector: (T) -> R): T? {
+        return maxByOrNull(selector)
+    }
+*/
