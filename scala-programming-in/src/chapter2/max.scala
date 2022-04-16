12bb8b16db833a01b90eab64f4e0cc5389d94b6e
棟幹 尹鎭煜 <71953982+y00njinuk@users.noreply.github.com>
2022-04-16 13:09:37 +0900

    fix Scala exercises

--- scala-programming-in/src/chapter2/max.scala
+++ scala-programming-in/src/chapter2/max.scala
@@ -3,7 +3,7 @@ package chapter2
 object max {
   def max_s1(a: Int, b: Int) { return }
   def max_s2(a: Int, b: Int) { if(a>b) a else b }
-  def max_s3(a: Int, b: Int): Int { if(a>b) a else b } // 잘못된 상단 구문 선언 (need '=')
+//  def max_s3(a: Int, b: Int): Int { if(a>b) a else b } // 잘못된 상단 구문 선언 (need '=')
 
   def max_e1(a: Int, b: Int): Int = { return if (a > b) a else b }
   def max_e2(a: Int, b: Int): Int = { if (a > b) a else b }
