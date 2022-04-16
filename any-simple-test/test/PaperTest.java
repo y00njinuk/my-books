b3bc68142bced522a474ae03cdab90736580ac8b
棟幹 尹鎭煜 <71953982+y00njinuk@users.noreply.github.com>
2022-04-16 14:46:31 +0900

    fix PaperTest

--- any-simple-test/test/PaperTest.java
+++ any-simple-test/test/PaperTest.java
@@ -2,17 +2,51 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @see dev.to/tlylt/compile-time-type-vs-run-time-type-oop-java-3-5fgn
+ */
 class PaperTest {
-
     @Test
     void test() {
         Paper paper = new Paper(297, 140);
         Object object = paper;
 
-        assertInstanceOf(Paper.class, paper);
-        assertInstanceOf(Object.class, paper);
+        object.equals( object ); // A
+        /**
+         * compile-time? {@link Object#equals(Object)}
+         * run-time? {@link Paper#equals(Object)}
+         */
 
-        assertInstanceOf(Paper.class, object);
-        assertInstanceOf(Object.class, object);
+        object.equals( (Paper) object ); // A
+        /**
+         * compile-time? {@link Object#equals(Object)}
+         * run-time? {@link Paper#equals(Object)}
+         *              => Why? Paper is subclass of Object
+         */
+
+        object.equals( paper ); // A
+        /**
+         * compile-time? {@link Object#equals(Object)}
+         * run-time? {@link Paper#equals(Object)}
+         *              => Why? Paper is subclass of Object
+         */
+
+        paper.equals( object ); // A
+        /**
+         * compile-time? {@link Paper#equals(Object)}
+         * run-time? {@link Paper#equals(Object)}
+         */
+
+        paper.equals( (Paper) object ); // B
+        /**
+         * compile-time? {@link Paper#equals(Paper)}
+         * run-time? {@link Paper#equals(Paper)}
+         */
+
+        paper.equals( paper ); // B
+        /**
+         * compile-time? {@link Paper#equals(Paper)}
+         * run-time? {@link Paper#equals(Paper)}
+         */
     }
 }
