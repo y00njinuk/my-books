fdb155bba3c042416aa52664bcd8cecf0c5c6572
棟幹 尹鎭煜 <71953982+y00njinuk@users.noreply.github.com>
2022-06-19 06:13:38 +0900

    add Java exercises for item8

--- effective-java/src/chapter2/item8/Room.java
+++ effective-java/src/chapter2/item8/Room.java
@@ -6,21 +6,6 @@ import java.lang.ref.Cleaner;
 public class Room implements AutoCloseable {
     private static final Cleaner cleaner = Cleaner.create();
 
-    // Resource that requires cleaning. Must not refer to Room!
-    private static class State implements Runnable {
-        int numJunkPiles; // Number of junk piles in this room
-
-        State(int numJunkPiles) {
-            this.numJunkPiles = numJunkPiles;
-        }
-
-        // Invoked by close method or cleaner
-        @Override public void run() {
-            System.out.println("Cleaning room");
-            numJunkPiles = 0;
-        }
-    }
-
     // The state of this room, shared with our cleanable
     private final State state;
 
@@ -34,5 +19,20 @@ public class Room implements AutoCloseable {
 
     @Override public void close() {
         cleanable.clean();
+    }
+
+    // Resource that requires cleaning. Must not refer to Room!
+    private static class State implements Runnable {
+        int numJunkPiles; // Number of junk piles in this room
+
+        State(int numJunkPiles) {
+            this.numJunkPiles = numJunkPiles;
+        }
+
+        // Invoked by close method or cleaner
+        @Override public void run() {
+            System.out.println("Cleaning room");
+            numJunkPiles = 0;
+        }
     }
 }
