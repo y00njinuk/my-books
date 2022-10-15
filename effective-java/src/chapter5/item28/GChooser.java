8fc2a29ead4ce07f803fed575e168312fab6f9fc
y00njinuk <jinuknamja@gmail.com>
2022-10-15 21:10:49 +0900

    add Java exercises for item28

--- effective-java/src/chapter5/item28/GChooser.java
+++ effective-java/src/chapter5/item28/GChooser.java
@@ -1,36 +1,38 @@
 package chapter5.item28;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Chooser {
-    private final Object[] choiceArray;
+// List-based Chooser - typesafe (Page 129)
+public class GChooser<T> {
+    private final List<T> choiceList;
 
-    public Chooser(Collection choices) {
-        choiceArray = choices.toArray();
+    public GChooser(Collection<T> choices) {
+        choiceList = new ArrayList<>(choices);
     }
 
-    public Object choose() {
+    public T choose() {
         Random rnd = ThreadLocalRandom.current();
-        return choiceArray[rnd.nextInt(choiceArray.length)];
+        return choiceList.get(rnd.nextInt(choiceList.size()));
     }
 
     public static void main(String[] args) {
         List<Integer> intList = List.of(1, 2, 3, 4, 5, 6);
-        Chooser intChooser = new Chooser(intList);
+        GChooser<Integer> intGChooser = new GChooser<>(intList);
 
         for (int i = 0; i < 10; i++) {
-            Number choice = (Number) intChooser.choose();
+            Number choice = intGChooser.choose();
             System.out.println(choice);
         }
 
         List<String> strList = List.of("a", "b", "c", "d", "e");
-        Chooser strChooser = new Chooser(strList);
+        GChooser<String> strGChooser = new GChooser<>(strList);
 
         for (int i = 0; i < 10; i++) {
-            String choice = (String) strChooser.choose();
+            String choice = strGChooser.choose();
             System.out.println(choice);
         }
     }
